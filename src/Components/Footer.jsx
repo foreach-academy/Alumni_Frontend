45108a7b1164d3629882e6ebce73eed2c53c0dfc
Ryan <ryandaltrey@hotmail.com>
2024-07-17 17:00:52 +0200

    ADD les liens et icons des reseaux sociaux sur le footer et leurs styles.

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -8,8 +8,10 @@ const Footer = () => {
            
                 <p className='navigation_footer'>LOGO</p>
                 <p className='navigation_footer'>CGU</p>
-                <a href='https://www.instagram.com/foreach_academy/' target='blank'><img src={require("../Assets/instagram_icon.png")} alt="instagram_icon"/></a>
-                <a href="https://www.linkedin.com/school/foreach-academy/"><img src={require("../Assets/linkedin_icon.png")} alt="linkedin_icon"/></a>
+                <div className='block_icon_reseaux_footer'>
+                <a href='https://www.instagram.com/foreach_academy/' target='blank' ><img src={require("../Assets/instagram_icon.png")} alt="instagram_icon" className='icon_reseaux_footer'/></a>
+                <a href="https://www.linkedin.com/school/foreach-academy/" target='blank' ><img src={require("../Assets/linkedin_icon.png")} alt="linkedin_icon" className='icon_reseaux_footer'/></a>
+                </div>
                 <p className='navigation_footer'>Mentions légales</p>
                 <p className='navigation_footer'>Confidentialité des données</p>
                 <div>
