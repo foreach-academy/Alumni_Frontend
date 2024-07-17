e9175c92a8bc2655a2e441409b9f3c45d151b11f
Ryan <ryandaltrey@hotmail.com>
2024-07-17 14:51:12 +0200

    ADD component NavBar et son Css pour la mise en forme.

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,22 @@
+import '../Styles/NavBar.css'
+
+const NavBar = () => {
+    return ( 
+       
+        <div className='navbar_block'>
+           
+                <p>X</p>
+                <p>Annuaire</p>
+                <p>Offres/Emploi</p>
+                <p>Evenements</p>
+                <p>Contacts</p>
+                <p>A propros</p>
+                <div>
+                <p>Profil</p>
+                <p>Espace connexion</p>
+                </div>
+        </div>
+    )
+}
+
+export default NavBar;
