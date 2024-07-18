f892db652cc83a15a4d6922ce4d397264df2d2fd
Ryan <ryandaltrey@hotmail.com>
2024-07-18 15:00:43 +0200

    Creation du fichier css pour InscriptionPage et son import, mise en place du titre de la page

--- src/Pages/InscriptionPage.jsx
+++ src/Pages/InscriptionPage.jsx
@@ -1,12 +1,13 @@
 import Footer from "../Components/Footer"
+import '../Styles/InscriptionPage.css'
 
 const PageInscription = () => {
 
 
 return <>
 
-    <div>
-        <h3>Vous êtes</h3>
+    <div className="titre_contenu">
+        <h3>Vous êtes :</h3>
     </div>
     <div className="">
         <a href=""><p>Entreprise</p></a>
