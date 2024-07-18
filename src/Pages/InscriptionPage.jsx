a70ffd0b62a60491a175419e09a22e2c532a871a
Ryan <ryandaltrey@hotmail.com>
2024-07-18 14:50:10 +0200

    Add la fonction pour Pageinscription.

---
+++ src/Pages/InscriptionPage.jsx
@@ -0,0 +1,21 @@
+import Footer from "../Components/Footer"
+
+const PageInscription = () => {
+
+
+return <>
+
+    <div>
+        <h3>Vous êtes</h3>
+    </div>
+    <div className="">
+        <a href=""><p>Entreprise</p></a>
+        <a href=""><p>Apprenant</p></a>
+        <a href=""><p>Formateur</p></a>
+    </div>
+    <Footer/>
+    </>
+    
+}
+
+export default PageInscription;
