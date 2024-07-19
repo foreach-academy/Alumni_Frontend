12a0de4a89aa7307aa154f9396fda23d663823ac
RyanDaltrey <158264738+RyanDaltrey@users.noreply.github.com>
2024-07-19 11:40:01 +0200

    Correction faute d'orthographe A propos. (#13)

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -10,7 +10,7 @@ const NavBar = () => {
             <p className='navigation_navbar'>OFFRES STAGE/EMPLOI</p>
             <p className='navigation_navbar'>EVENEMENTS</p>
             <p className='navigation_navbar'>CONTACTS</p>
-            <p className='navigation_navbar'>A PROPROS</p>
+            <p className='navigation_navbar'>A PROPOS</p>
             <div>
                 <p className='navigation_navbar'>PROFIL</p>
                 <p className='navigation_navbar'>ESPACE CONNEXION</p>
