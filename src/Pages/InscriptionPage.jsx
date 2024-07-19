4f1e3efab541f9c388942d805e93a5cf656435db
RyanDaltrey <158264738+RyanDaltrey@users.noreply.github.com>
2024-07-19 11:39:08 +0200

    Page inscription (#12)
    * Creation de la page Inscription dans le dossier Pages.
    
    * Creation de la navigation pour inscription dans App.js
    
    * ADD element InscriptionPage dans la navigation de App.js
    
    * Add la fonction pour Pageinscription.
    
    * Creation du fichier css pour InscriptionPage et son import, mise en place du titre de la page
    
    * ADD le css sur le lien entreprise
    
    * ADD le css sur les liens Apprenant et Formation
    
    * ADD balise img pour le futur logo Alumni
    
    * Import et mise en place d'une image pour le background de la page.
    
    * Commenter Navbar dans App.js pour ne pas avoir la navbar sur la page inscription
    
    * Decommentation de la NavBar dans App.js

---
+++ src/Pages/InscriptionPage.jsx
@@ -0,0 +1,24 @@
+import Footer from "../Components/Footer"
+import '../Styles/InscriptionPage.css'
+
+const PageInscription = () => {
+
+
+return <>
+    <div className="logo_alumni_page_inscription">
+        <img src="" alt="LOGO" />
+    </div>
+    <div className="titre_contenu">
+        <h3>Vous êtes :</h3>
+    </div>
+    <div className="categorie_inscription">
+        <a href="" className="lien_categorie"><p className="categorie_entreprise">Entreprise</p></a>
+        <a href="" className="lien_categorie"><p className="categorie_apprenant">Apprenant</p></a>
+        <a href="" className="lien_categorie"><p className="categorie_formateur">Formateur</p></a>
+    </div>
+    <Footer/>
+    </>
+    
+}
+
+export default PageInscription;
