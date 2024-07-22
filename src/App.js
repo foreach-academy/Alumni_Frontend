80efb5dc38d369258d08c74a6ffe7b326df5294d
Ryan <ryandaltrey@hotmail.com>
2024-07-22 13:23:12 +0200

    Mise en place de la navigation pour la page inscription entreprise

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import NavBar from './Components/NavBar';
 import Footer from './Components/Footer';
 import InscriptionPage from './Pages/InscriptionPage';
+import InscriptionEntreprise from './Pages/InscriptionEntreprisePage';
 
 function App() {
   return<>
@@ -10,6 +11,7 @@ function App() {
   <Routes>
     <Route path='/'/>
     <Route path='/inscription' element={<InscriptionPage/>}/>
+    <Route path='/inscription_entreprise' element={<InscriptionEntreprise/>}/>
   </Routes>
   <Footer/>
   </BrowserRouter>
