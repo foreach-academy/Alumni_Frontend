fdfb0c9a6a3fb59856355e24e6e37ca32573bfe9
Ryan <ryandaltrey@hotmail.com>
2024-07-22 12:56:37 +0200

    Import de l'image background pour la page inscription apprenant et ajout de la className dans le body, ainsi modification de l'ortographe des placeholders.

--- src/Pages/InscriptionApprenant.jsx
+++ src/Pages/InscriptionApprenant.jsx
@@ -38,19 +38,21 @@ const InscriptionApprenant = () => {
 
 
 return <>
+<body className="page_inscription_apprenant">
     <div className="content_logo_page_inscription_apprenant">
         <img src={require("../Assets/logo_foreach_couleur_horizontal.png")} alt="logo_foreach" className="logo_foreach_page_inscription_apprenant" />
     </div>
     <div className="block_inscription_apprenant">
-        <input type="email" name="email" defaultValue={email} placeholder="email" onChange={(e) => {setEmail(e.target.value)}} className="input_inscription_apprenant"/>
-        <input type="password" name="mot de passe" defaultValue={mdp} placeholder="mot de passe" onChange={(e) => {setMdp(e.target.value)}} className="input_inscription_apprenant"/>
-        <input type="text" name="nom" defaultValue={nom} placeholder="nom" onChange={(e) => {setNom(e.target.value)}} className="input_inscription_apprenant"/>
-        <input type="text" name="prenom" defaultValue={prenom} placeholder="prenom" onChange={(e) => {setPrenom(e.target.value)}}className="input_inscription_apprenant" />
+        <input type="email" name="email" defaultValue={email} placeholder="Email" onChange={(e) => {setEmail(e.target.value)}} className="input_inscription_apprenant"/>
+        <input type="password" name="mot de passe" defaultValue={mdp} placeholder="Mot de passe" onChange={(e) => {setMdp(e.target.value)}} className="input_inscription_apprenant"/>
+        <input type="text" name="nom" defaultValue={nom} placeholder="Nom" onChange={(e) => {setNom(e.target.value)}} className="input_inscription_apprenant"/>
+        <input type="text" name="prenom" defaultValue={prenom} placeholder="Prenom" onChange={(e) => {setPrenom(e.target.value)}}className="input_inscription_apprenant" />
 
         <input type="text" name="formation" defaultValue={formation} placeholder="Formation" onChange={(e) => {setFormation(e.target.value)}} className="input_inscription_apprenant"/>
         <input type="text" name="promotion" defaultValue={promotion} placeholder="Promotion" onChange={(e) => {setPromotion(e.target.value)}}className="input_inscription_apprenant"/>
         <button onClick={() => {inscription()}} className="boutton_inscription_apprenant">Valider</button>
     </div>
+</body>
     <Footer/>
     </>
     
