7377d1eb370afed23152b9d6ca96eb5433d9e2ae
RyanDaltrey <158264738+RyanDaltrey@users.noreply.github.com>
2024-07-30 16:57:21 +0200

    Inscription (#24)
    * Modification erreur
    
    * ADD le size des inputs dans les pages inscriptions eleve, entreprise et formateur et ajout de la fonctionalité inscription dans le onclick des bouttons valider dans les pages inscriptions.
    
    * regletement de div en trop
    
    * Modification de la const inscription sur la page formateur
    
    * Modification de la structure des pages inscriptions et leurs style.

--- src/Pages/InscriptionApprenant.jsx
+++ src/Pages/InscriptionApprenant.jsx
@@ -56,54 +56,39 @@ return <>
              className="logo_foreach_page_inscription_apprenant"/>
     </div>
     <div className="block_inscription_apprenant">
-        inscription
+
         <div className="premiere_partie_input">
-            <input type="email" name="email" defaultValue={email} placeholder="Email" onChange={(e) => {
-                setEmail(e.target.value)
-            }} className="input_inscription_apprenant" required/>
-            <input type="password" name="mot de passe" defaultValue={mdp} placeholder="Mot de passe" onChange={(e) => {
-                setMdp(e.target.value)
-            }} className="input_inscription_apprenant" required/>
-            <input type="text" name="nom" defaultValue={nom} placeholder="Nom" onChange={(e) => {
-                setNom(e.target.value)
-            }} className="input_inscription_apprenant" required/>
+            <input type="email" name="email" defaultValue={email} placeholder="Email" onChange={(e) => {setEmail(e.target.value)}} className="input_inscription_apprenant" size={35} required/>
+            <input type="password" name="mot de passe" defaultValue={mdp} placeholder="Mot de passe" onChange={(e) => {setMdp(e.target.value)}} className="input_inscription_apprenant" required/>
+            <input type="text" name="nom" defaultValue={nom} placeholder="Nom" onChange={(e) => {setNom(e.target.value)}} className="input_inscription_apprenant" required/>
         </div>
         <div className="deuxieme_partie_input">
-            <input type="text" name="prenom" defaultValue={prenom} placeholder="Prenom" onChange={(e) => {
-                setPrenom(e.target.value)
-            }} className="input_inscription_apprenant" required/>
-            <input type="text" name="formation" defaultValue={formation} placeholder="Formation" onChange={(e) => {
-                setFormation(e.target.value)
-            }} className="input_inscription_apprenant" required/>
-            <input type="text" name="promotion" defaultValue={promotion} placeholder="Promotion" onChange={(e) => {
-                setPromotion(e.target.value)
-            }} className="input_inscription_apprenant" required/>
+            <input type="text" name="prenom" defaultValue={prenom} placeholder="Prenom" onChange={(e) => {setPrenom(e.target.value)}}className="input_inscription_apprenant" size={35} required />
+            <input type="text" name="formation" defaultValue={formation} placeholder="Formation" onChange={(e) => {setFormation(e.target.value)}} className="input_inscription_apprenant" required/>
+            <input type="text" name="promotion" defaultValue={promotion} placeholder="Promotion" onChange={(e) => {setPromotion(e.target.value)}}className="input_inscription_apprenant" required/>
+        </div> 
         </div>
-    </div>
-    <div className="block_case_a_cocher_apprenant">
-        <div className="case_a_cocher_en_formation">
-            <input type="checkbox" required/>
-            <label for="confirmer_en_formation">Je suis actuellement apprenant·e</label>
-        </div>
-
-        <div className="case_a_cocher_inscription_apprenant">
-            <input type="checkbox" required/>
-            <label for="valider_inscription">En m'inscrivant, j'accepte que les données renseignées soient utilisées par
-                l'équipe de ForEach Academy.</label>
-        </div>
-    </div>
-    <div className="block_boutton_inscription_apprenant">
-        <button onClick={validate} className="boutton_inscription_apprenant">Validate</button>
-    </div>
-    <div>
-
-        {emailError && <p>Votre email est invalide</p>}
-        {mdpError && <p>Votre mot de passe est invalide</p>}
-
-
-    </div>
-
-    </body>
+        <div className="block_case_a_cocher_apprenant">
+            <div className="case_a_cocher_en_formation">
+                <input type="checkbox" required/>
+                <label for="confirmer_en_formation">Je suis actuellement apprenant·e</label>
+            </div>
+            
+            <div className="case_a_cocher_inscription_apprenant">
+                <input type="checkbox" required/>
+                <label for="valider_inscription">En m'inscrivant, j'accepte que les données renseignées soient utilisées par l'équipe de ForEach Academy.</label>
+            </div>
+        </div> 
+            <div className="block_boutton_inscription_apprenant">
+                <button onClick={validate && inscription} className="boutton_inscription_apprenant" >Validate</button>
+            </div>
+            {emailError && <p>Votre email est invalide</p>}
+            {mdpError && <p>Votre mot de passe est invalide</p>}
+   
+      
+    
+          
+</body>
     <Footer/>
     </>
     
