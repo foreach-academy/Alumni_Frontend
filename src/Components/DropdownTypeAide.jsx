3d756ac40fa5433fef5edb5daafa1dd200d51272
Ryan <ryandaltrey@hotmail.com>
2024-08-08 20:10:51 +0200

    Add dropdown pour typeaide

---
+++ src/Components/DropdownTypeAide.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import Select from 'react-select';
+import TypeAideService from "../Services/TypeAideService";
+
+const TypeAide = () => {
+    const [typeAide, setTypeAide] = useState([]);
+
+    const fetchTypeAide = async () => {
+        try {
+            const response = await TypeAideService.getAllTypeAide();
+            setTypeAide(response.data);
+        } catch (error) {
+            console.error('Error fetching type aide:', error);
+        }
+    };
+
+    useEffect(() => {
+        fetchTypeAide();
+    }, []);
+
+    const options = typeAide.map(type_aide => ({
+        value: type_aide.type_aide,
+        label: type_aide.type_aide,
+    }));
+
+    return (
+        <Select
+            options={options}
+            className="dropdown_type_aide"
+        />
+    );
+};
+
+export default TypeAide;
