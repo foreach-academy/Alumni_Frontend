1e70ea0c91717b6bffe2f8527f12b508b0752235
Ryan <ryandaltrey@hotmail.com>
2024-08-08 15:56:33 +0200

    ADD profil Service pour get all prodil er get profilById

--- src/Services/ProfilService.js
+++ src/Services/ProfilService.js
@@ -1,8 +1,12 @@
 import axios from "axios";
 
 class   ProfilService {
-    static getAllFormation(){
-        return axios.get("http://127.0.0.1:3000/formation/profil");
+    static getAllProfil(){
+        return axios.get("http://127.0.0.1:3000/profil");
+    }
+
+    static getProfilById(id){
+        return axios.get("http://127.0.0.1:3000/formation/profil/" + id);
     }
 }
 
