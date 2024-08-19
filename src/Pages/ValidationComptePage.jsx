0d24837e5b0ead0a72ef493a414e2a75f14fd6be
Rida Ouajib <158264725+Ridano97@users.noreply.github.com>
2024-08-19 14:45:44 +0200

    Validation compte page (#29)
    * Création de la page ValidationComptePage
    
    * création de la page validation par admin manque la récupération de l'inscription

--- src/Pages/ValidationComptePage.jsx
+++ src/Pages/ValidationComptePage.jsx
@@ -1,4 +1,9 @@
 import React from 'react'
+import NavBar from '../Components/NavBar';
+import Footer from '../Components/Footer';
+import '../Styles/ValidationCompte.css'
+import ButtonValide from '../Assets/valide.png';
+import ButtonRefuse from '../Assets/croix.png';
 
 const ValidationComptePage = () => {
 
@@ -6,11 +11,45 @@ const ValidationComptePage = () => {
     
         <NavBar/>
         <main>
-            
+            <h1 id='title-validation'>INSCRIPTION À VALIDER</h1>
+        <div className='input-container'>
+            <ul className='information-user'>
+                <li className='items-informations'>Nom</li>
+                <li className='items-informations'>Prénom</li>
+                <li className='items-informations'>Email</li>
+                <li className='items-informations'>Formation</li>
+                <li className='items-informations'>Promotion</li>
+            </ul>
+            <div className='block-button-validate'>
+                <button className='style-button'><img src={ButtonValide} alt="logocheck" height={15} width={15} /></button>
+                <button className='style-button'><img src={ButtonRefuse} alt="logocroix" height={15} width={15} /></button>
+            </div>
+        </div>
+        <div className='input-container'>
+            <ul className='information-user'>
+                <li className='items-informations'>Nom</li>
+                <li className='items-informations'>Prénom</li>
+            </ul>
+            <div className='block-button-validate'>
+                <button className='style-button'><img src={ButtonValide} alt="logocheck" height={15} width={15} /></button>
+                <button className='style-button'><img src={ButtonRefuse} alt="logocroix" height={15} width={15} /></button>
+            </div>
+        </div>
+        <div className='input-container'>
+            <ul className='information-user'>
+                <li className='items-informations'>Nom</li>
+                <li className='items-informations'>Prénom</li>
+                <li className='items-informations'>Email</li>
+                <li className='items-informations'>Formation</li>
+                <li className='items-informations'>Promotion</li>
+            </ul>
+            <div className='block-button-validate'>
+                <button className='style-button'><img src={ButtonValide} alt="logocheck" height={15} width={15} /></button>
+                <button className='style-button'><img src={ButtonRefuse} alt="logocroix" height={15} width={15} /></button>
+            </div>
+        </div>
         </main>
         <Footer/>
-
-    
     </>
 }
 
